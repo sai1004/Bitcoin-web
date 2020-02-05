c78bc5dcdfd32a79a51e122e4869fd572d69c5f7
sai1004 <saikiran.bavandla@gmail.com>
2020-02-05 09:30:49 +0530

    commit

--- src/app/components/what-bitcoin/what-bitcoin.component.ts
+++ src/app/components/what-bitcoin/what-bitcoin.component.ts
@@ -10,7 +10,7 @@ export class WhatBitcoinComponent implements OnInit {
     {
       head: "What is Bitcoin",
       subHead:
-        "Bitcoin is an innovative payment network and a new kind of money.",
+        "Bitcoin is an innovative payment network and a new <wb> kind of money.",
       para:
         "Bitcoin is one of the most important inventions in all of <br/> human history. For the first time ever, anyone can send or <br/> receive any amount of money with anyone else, anywhere <br/> on the planet, conveniently and without restriction. It’s <br/> the dawn of a better, more free world."
     }
