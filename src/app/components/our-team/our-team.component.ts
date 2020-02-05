1f1731e4b6f921401101934f1c444c96ace53335
sai1004 <saikiran.bavandla@gmail.com>
2020-02-05 20:53:05 +0530

    commit

--- src/app/components/our-team/our-team.component.ts
+++ src/app/components/our-team/our-team.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-our-team',
-  templateUrl: './our-team.component.html',
-  styleUrls: ['./our-team.component.css']
+  selector: "app-our-team",
+  templateUrl: "./our-team.component.html",
+  styleUrls: ["./our-team.component.css"]
 })
 export class OurTeamComponent implements OnInit {
+  team: any[] = [
+    {
+      img: "../../../assets/img/member/1.jpg",
+      name: "Tom Binegar",
+      designation: "Business Devlopment"
+    },
+    {
+      img: "../../../assets/img/member/2.jpg",
+      name: "Tom Binegar",
+      designation: "Business Devlopment"
+    },
+    {
+      img: "../../../assets/img/member/3.jpg",
+      name: "Tom Binegar",
+      designation: "Business Devlopment"
+    },
+    {
+      img: "../../../assets/img/member/4.jpg",
+      name: "Tom Binegar",
+      designation: "Business Devlopment"
+    },
+    {
+      img: "../../../assets/img/member/5.jpg",
+      name: "Tom Binegar",
+      designation: "Business Devlopment"
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
